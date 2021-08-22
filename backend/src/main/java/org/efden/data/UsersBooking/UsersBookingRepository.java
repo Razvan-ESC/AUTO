6642abf4023762d9050082f49e01a8dcd83d3e6a
RazvanHilea <razvan.hilea@yahoo.com>
2021-08-22 16:45:53 +0300

    Done backend for now. No stats yet

---
+++ backend/src/main/java/org/efden/data/UsersBooking/UsersBookingRepository.java
@@ -0,0 +1,6 @@
+package org.efden.data.UsersBooking;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersBookingRepository extends CrudRepository<UsersBookingClass, Integer> {
+}
